5926f9e641715b6ad93c7df72b00171b966e211c
Minu James <minupjames@gmail.com>
2021-02-10 19:49:02 -0800

    Fix window reload on cash transfer

--- static/src/js/cash_transfer.js
+++ static/src/js/cash_transfer.js
@@ -14,28 +14,24 @@ $(document).ready(function(){
 			var des_account_value = e.options[e.selectedIndex].text
 			var s = document.getElementById("source_account_id");
 			var src_account_value = e.options[s.selectedIndex].text
-			const GXHR = new XMLHttpRequest();
-			GXHR.addEventListener("load", function() {
-				$('body').html(GXHR.responseText);
-				$('label[for="date"]').hide();
-				$('input[name="date"]').hide();
-				$('label[for="note"]').hide();
-				$('input[name="note"]').hide();
-				$('label[for="source_account"]').hide();
-				$('#source_account').hide();
-				$('label[for="amount"]').hide();
-				$('#amount').hide();
-				$('label[for="destination_account"]').hide();
-				$('#dest_account_id').hide();
-				$('#source_account_id').hide();
-				$('button').hide();
-				$('.alert').show();
-				$("#dest_account").text(des_account_value);
-				$("#src_account").text(src_account_value);
-			});
-            GXHR.open( "GET", "/cash_transfer", true);
-            GXHR.send();
+            $('label[for="date"]').hide();
+			$('input[name="date"]').hide();
+			$('label[for="note"]').hide();
+			$('input[name="note"]').hide();
+			$('label[for="source_account"]').hide();
+			$('#source_account').hide();
+			$('label[for="amount"]').hide();
+			$('#amount').hide();
+			$('label[for="destination_account"]').hide();
+			$('#dest_account_id').hide();
+			$('#source_account_id').hide();
+			$('button').hide();
+			$('.alert').show();
+			setTimeout(function(){ $('.alert').show(); }, 3000);
+			$("#dest_account").text(des_account_value);
+			$("#src_account").text(src_account_value);
 			window.history.pushState({}, null, "/cash_transfer");
+			window.location.href = '/my_expenses';
 		});
 		
 		XHR.open( "POST", "/cash_transfer", true);
